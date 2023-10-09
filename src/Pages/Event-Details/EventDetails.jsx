fe13e7b1e036e23776de3b71c876fafadb4dc640
shathi <shathi@gmail.com>
2023-10-09 12:50:14 +0600

    get single event data in detals page

--- src/Pages/Event-Details/EventDetails.jsx
+++ src/Pages/Event-Details/EventDetails.jsx
@@ -4,8 +4,10 @@ import { useLoaderData, useParams } from "react-router-dom";
 const EventDetails = () => {
     const { eventId } = useParams()
     const Events = useLoaderData()
-    console.log(eventId)
-    console.log(Events)
+    
+    const event = Events.find(event => event.eventID == eventId)
+    console.log(event)
+
     return (
         <div>
             <h1>Event Details page</h1>
